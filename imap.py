c870167a1eb2bb688ea3bbb4fbba4b1c0d4095f1
Francesco Gigli <jaramir@slack.it>
2011-12-14 19:00:19 +0100

    first commit

---
+++ imap.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; -*-
+# IMAP Server Tester
+# Copyright © 2011 Francesco Gigli <jaramir@gmail.com>
+
+import imaplib
+import argparse
+
+# parse args
+
+parser = argparse.ArgumentParser( description = "SMTP Testing Utility" )
+    
+parser.add_argument( "username",   action="store",      help="Username" )
+parser.add_argument( "password",   action="store",      help="Password" )
+parser.add_argument( "server",     action="store",      help="IMAP Server Hostname or IP" )
+parser.add_argument( "message",    action="store",      help="Message ID to read", default=None, nargs="?" )
+parser.add_argument( "--folder",   action="store",      help="IMAP Folder to test (defualt: INBOX)" )
+parser.add_argument( "--port",     action="store",      help="IMAP Server TCP Port (default: 143 or 993)" )
+#parser.add_argument( "--tls",      action="store_true", help="Use TLS to encrypt traffic" )
+parser.add_argument( "--ssl",      action="store_true", help="Use SSL to encrypt the connection" )
+parser.add_argument( "--cram-md5", action="store_true", help="Use CRAM-MD5 authentication" )
+
+args = parser.parse_args()
+
+# set debug level
+
+imaplib.Debug = 4
+
+# connect
+
+if args.ssl:
+    port = args.port if args.port else 993 
+    imap = imaplib.IMAP4_SSL( args.server, port )
+else:
+    port = args.port if args.port else 143
+    imap = imaplib.IMAP4( args.server, port )
+
+#if args.tls:
+#    imap.starttls()
+
+# auth
+
+if args.cram_md5:
+    imap.login_cram_md5( args.username, args.password )
+else:
+    imap.login( args.username, args.password )
+
+# test
+
+folder = args.folder if args.folder else "INBOX"
+imap.select( folder )
+
+if args.message:
+    typ, data = imap.fetch( args.message, '(RFC822)' )
+    print typ
+    print data[0][0]
+    print data[0][1] 
+else:
+    typ, data = imap.search( None, "ALL" )
+    print " ".join( data )
+
+imap.close()
+imap.logout()
---
+++ smtp.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; -*-
+# SMTP Server Tester
+# Copyright © 2011 Francesco Gigli <jaramir@gmail.com>
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import email.utils
+import argparse
+
+# parse args
+
+parser = argparse.ArgumentParser( description = "SMTP Testing Utility" )
+    
+parser.add_argument( "from_addr", action="store",      help="Email FROM Address" )
+parser.add_argument( "rcpt_addr", action="store",      help="Email RCPT Address" )
+parser.add_argument( "server",    action="store",      help="SMTP Server Hostname or IP" )
+parser.add_argument( "port",      action="store",      help="SMTP Server TCP Port", default=25, nargs="?" )
+parser.add_argument( "--gtube",   action="store_true", help="Send GTUBE AntiSpam Test" )
+parser.add_argument( "--eicar",   action="store_true", help="Send EICAR AntiVirus Test" )
+parser.add_argument( "--tls",     action="store_true", help="Use TLS to encrypt traffic" )
+parser.add_argument( "--ssl",     action="store_true", help="Use SSL to encrypt the connection" )
+parser.add_argument( "--auth",    action="store",      help="Use SMTP AUTH with provided credentials",
+                                                                                    default=False, nargs=2, metavar=( "USERNAME", "PASSWORD" ) )
+parser.add_argument( "--subject", action="store",      help="Email Subject",        default="Test message" )
+parser.add_argument( "--text",    action="store",      help="Email Text",           default="This is a test message" )
+
+args = parser.parse_args()
+
+# compose message
+
+msg = MIMEMultipart()
+msg["Subject"] = args.subject
+msg["From"] = args.from_addr
+msg["To"] = args.rcpt_addr
+msg["Date"] = email.utils.formatdate()
+
+msg.attach( MIMEText( args.text ) )
+
+if args.gtube:
+    msg.attach( MIMEText( "XJS*C4JDBQADN1.NSBN3*2IDNEN*GTUBE-STANDARD-ANTI-UBE-TEST-EMAIL*C.34X" ) )
+
+if args.eicar:
+    msg.attach( MIMEText( "X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*" ) )
+
+# connect and send
+
+if args.ssl:
+    smtp = smtplib.SMTP_SSL( args.server, args.port )
+else:
+    smtp = smtplib.SMTP( args.server, args.port )
+
+smtp.set_debuglevel( True )
+
+if args.tls:
+    smtp.starttls()
+
+if args.auth:
+    username, password = args.auth
+    smtp.login( username, password )
+
+smtp.sendmail( args.from_addr, args.rcpt_addr, msg.as_string() )
+smtp.quit()
